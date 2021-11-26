7fb01287a6bea0cc6ce3018ee698e42abcfcc3a5
vuquangtuan123 <vuquangtuan12345@gmail.com>
2021-11-26 22:26:10 +0700

    feat: add detail page

---
+++ src/components/Detail.js
@@ -0,0 +1,142 @@
+import React from "react";
+import styled from "styled-components";
+
+function Detail() {
+  return (
+    <Container>
+      <Background>
+        <img
+          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/9D8AEB7DE234898392BFD20E7D9B112B841E920AF9A3F54CCFB966722AFF3461/scale?width=1920&aspectRatio=1.78&format=jpeg"
+          alt=""
+        />
+      </Background>
+      <ImageTitle>
+        <img
+          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/EF737B93E2F2ABE27C74CBBEB322F18A421E7986129E9989587CEF2295B0447F/scale?width=1344&aspectRatio=1.78&format=png"
+          alt=""
+        />
+      </ImageTitle>
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" alt="" />
+          <span>PLAY</span>
+        </PlayButton>
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" alt="" />
+          <span>TRAILER</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupWatchButton>
+          <img src="/images/group-icon.png" alt="" />
+        </GroupWatchButton>
+      </Controls>
+      <Subtitle>2018 ● 7m ● Family, Fantasy, Kids, Animation</Subtitle>
+      <Description>Hello World</Description>
+    </Container>
+  );
+}
+
+export default Detail;
+
+const Container = styled.div`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+`;
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  opacity: 0.8;
+  z-index: -1; /* set position behind header */
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const ImageTitle = styled.div`
+  padding: 50px 0 50px;
+  height: 30vh;
+  width: 35vw;
+  min-height: 170px;
+  min-width: 200px;
+
+  img {
+    height: 100%;
+    object-fit: contain;
+  }
+`;
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PlayButton = styled.button`
+  cursor: pointer;
+  border-radius: 4px;
+  font-size: 15px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background-color: rgb(249, 249, 249);
+  border: none;
+  padding: 0 24px;
+  margin-right: 22px;
+  letter-spacing: 1.8px;
+  text-transform: uppercase;
+
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+`;
+
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+`;
+
+const AddButton = styled.button`
+  margin-right: 16px;
+  cursor: pointer;
+  width: 44px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background: rgba(0, 0, 0, 0.6);
+
+  span {
+    font-size: 32px;
+    color: #fff;
+  }
+`;
+
+const GroupWatchButton = styled(AddButton)`
+  background: rgb(0, 0, 0);
+`;
+
+const Subtitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 25px;
+`;
+
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 16px;
+  color: rgb(249, 249, 249);
+`;
